8866be65ec5a312b82b28c9d6378b4ff67c6415d
galihmrd <galihmardiana15@gmail.com>
2021-11-25 01:33:20 +0000

    Revert "fwb: Port long screenshot implementation from Asus"
    This reverts commit 5592e5b80c4ee2d1395707e2f8190c2715c6bf3d.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6623,10 +6623,4 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    @Override
-    public void takeAlternativeScreenshot() {
-        mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
-        mHandler.post(mScreenshotRunnable);
-    }
-
 }
